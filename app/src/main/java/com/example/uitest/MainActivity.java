8bcb628afbd7304df28962fbac64d1b0a2f78f41
leiyao6 <leiyao6@iflytek.com>
2021-04-19 18:52:27 +0800

    研究一下shape的用法

--- app/src/main/java/com/example/uitest/MainActivity.java
+++ app/src/main/java/com/example/uitest/MainActivity.java
@@ -7,14 +7,17 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 private Button btn_test;
 private EditText editText;
+private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn_test = findViewById(R.id.btn_test);
          editText = findViewById(R.id.edit_text);
         btn_test.setOnClickListener(new View.OnClickListener() {
